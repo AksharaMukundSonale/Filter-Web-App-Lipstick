1b3960d8fab456a7874747e9dd94f4ee6afd4dce
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2023-12-10 13:41:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
-function preload(){
+lipstickX=0;
+lipstickY=0;
+
+function preload() {
+  lipstick = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
 }
 
-function setup()
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
 {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300, 300);
-    video.hide()
+  if(results.length > 0)
+  {
+    console.log(results);
+   lipstickX = results[0].pose.nose.x -30;
+    lipstickY = results[0].pose.nose.y +10;
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+  }
 }
 
-function modelLoaded()
-{
-    console.log('PoseNet is initialized');
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(lipstick, lipstickX, lipstickY, 50, 60);
 }
 
-function gotPoses()
-{
-    if(results.length > 0)
-    {
-        console.log(results)
-        console.log("lips x = " + results[0].pose.lips.x);
-        console.log("lips y = " + results[0].pose.lips.y);
-    }
-}
-
-function draw(){
-    image(video, 0, 0, 300, 300);
-}
-
-function take_snapshot()
-{
-    save('myLipsticFilter.png');
+function take_snapshot(){    
+  save('myLipstickFilterImage.png');
 }
